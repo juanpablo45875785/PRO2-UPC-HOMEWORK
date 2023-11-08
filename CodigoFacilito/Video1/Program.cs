0ad4e4797e008af3d7eb8c77aae7130e562971c9
Juan Pablo Tapeosi Chamo <jtapeosichamo@gmail.com>
2023-11-07 23:44:18 -0400

    tarea

---
+++ CodigoFacilito/Video1/Program.cs
@@ -0,0 +1,21 @@
+using system;
+using system.Collections.Generic;
+using system.Linq;
+using system.Text;
+using system.Threading.Tasks;
+
+
+namespace Video1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // este es un programa de hola mundo
+            /*este es un comentario 
+            de bloque */
+            console.WriteLine("Hola Mundo");
+            console.ReadKey();
+        }
+    }
+}
