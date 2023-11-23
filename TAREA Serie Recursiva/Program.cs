8b2a86784a03176a70b234cf52daa9a629ea6055
Juan Pablo Tapeosi Chamo <52146627+juanpablo45875785@users.noreply.github.com>
2023-11-23 15:37:03 -0400

    ITERATIVO RECURSIVA

--- TAREA Serie Recursiva/Program.cs
+++ TAREA Serie Recursiva/Program.cs
@@ -1,48 +1,98 @@
-﻿using System;
-
-public class Program
+﻿public class Program
 {
-    public static void Main(string[]args)
+    public static void Main(string[] args)
     {
-        // Imprimir los primeros 10 términos de la serie
+        Console.WriteLine("CODIGO: Iterativo");
+        // Llama a la función para generar la serie Iteractivo con los primeros 10 términos
+        GenerarSerieIterativo(10);
 
-        // Bucle for que itera desde 1 hasta 10
-        for (int i = 1; i <= 10; i++)
-        {
-            // Llama a la función RecursivaSerie y muestra el resultado en la consola
-            Console.Write(RecursivaSerie(i));
-            
-            // Imprimir una coma y un espacio si no es el último número
-            if (i < 10)
-            {
-                Console.Write(",");
-            }
-        }
+        // Imprime un salto de línea para separar las dos secuencias
+        Console.WriteLine();
+        Console.WriteLine("CODIGO: Recursivo");
+
+        // Llama a la función para generar la serie recursiva con los primeros 10 términos
+        GenerarSerieRecursivo(10, 1);
+        
     }
-
-    // Función recursiva para calcular los términos de la serie
-    public static int RecursivaSerie(int a)
+    public static void GenerarSerieIterativo(int A)
+{
+    // Itera desde 1 hasta n
+    for (int i = 1; i <= A; i++)
     {
-        // Primer caso base: si n es igual a 1, devuelve 1
-        if (a == 1)
+        // Imprime el término generado por la función GenerarSerie
+        Console.Write(GenerarSerieI(i));
+
+        // Imprime una coma y un espacio si no es el último término
+        if (i < A)
         {
-            return 1;
-        }
-        // Segundo caso base: si a es igual a 2, devuelve 6
-        if (a == 2)
-        {
-            return 6;
-        }
-        // Tercer caso: si a es par, estamos en la segunda secuencia (6, 5, 4, ...)
-        if (a % 2 == 0)
-        {
-            return RecursivaSerie(a - 2) - 1;
-        }
-        // Cuarto caso: si a es impar, estamos en la primera secuencia (1, 2, 3, ...)
-        else
-        {
-            return RecursivaSerie(a - 2) + 1;
+            Console.Write(", ");
         }
     }
 }
 
+public static int GenerarSerieI(int A)
+{
+    // Caso base: si A es 1, devuelve 1
+    if (A == 1)
+    {
+        return 1;
+    }
+    // Caso base: si A es 2, devuelve 6
+    else if (A == 2)
+    {
+        return 6;
+    }
+    // Caso recursivo: si A es par, llama recursivamente restando 2 y resta 1 al resultado
+    else if (A % 2 == 0)
+    {
+        return GenerarSerieI(A - 2) - 1;
+    }
+    // Caso recursivo: si A es impar, llama recursivamente restando 2 y suma 1 al resultado
+    else
+    {
+        return GenerarSerieI(A - 2) + 1;
+    }
+}
+    public static void GenerarSerieRecursivo(int B, int i)
+    {
+        // Verifica si el índice actual es menor o igual al límite
+        if (i <= B)
+        {
+            // Llama a la función para generar el término actual e imprímirlo
+            Console.Write(GenerarTerminoR(i));
+
+            // Imprime una coma y un espacio si no es el último término
+            if (i < B)
+            {
+                Console.Write(", ");
+            }
+
+            // Llama recursivamente a la función con el siguiente índice
+            GenerarSerieRecursivo(B, i + 1);
+        }
+    }
+
+    public static int GenerarTerminoR(int B)
+    {
+        // Caso base: si B es 1, devuelve 1
+        if (B == 1)
+        {
+            return 1;
+        }
+        // Caso base: si B es 2, devuelve 6
+        else if (B == 2)
+        {
+            return 6;
+        }
+        // Caso recursivo segundo secuencia: si Ba es par, llama recursivamente restando 2 y resta 1 al resultado
+        else if (B % 2 == 0)
+        {
+            return GenerarTerminoR(B - 2) - 1;
+        }
+        // Caso recursivo primera secuencia: si B es impar, llama recursivamente restando 2 y suma 1 al resultado
+        else
+        {
+            return GenerarTerminoR(B - 2) + 1;
+        }
+    }
+}
